60ddbd5ad57f0e76eb36bec6029a0aaf334a6279
Stuart Turner <stuart@turner-isageek.com>
2024-05-21 09:57:09 -0500

    Additional testing

---
+++ tests/Immediate.Validations.Tests/GeneratorTests/CustomValidationTests.AdditionalValidationsInherited#IV...BaseClass.g.verified.cs
@@ -0,0 +1,40 @@
+﻿//HintName: IV...BaseClass.g.cs
+using System.Collections.Generic;
+using Immediate.Validations.Shared;
+
+#nullable enable
+#pragma warning disable CS1591
+
+
+partial class BaseClass
+{
+	static List<ValidationError> IValidationTarget<BaseClass>.Validate(BaseClass? target) =>
+		Validate(target);
+
+	public static  List<ValidationError> Validate(BaseClass? target)
+	{
+		if (target is not { } t)
+		{
+			return 
+			[
+				new()
+				{
+					PropertyName = ".self",
+					ErrorMessage = "`target` must not be `null`.",
+				},
+			];
+		}
+		
+		var errors = new List<ValidationError>();
+
+
+
+		errors.AddRange(AdditionalValidations(t));
+
+		return errors;
+	}
+
+
+
+}
+
---
+++ tests/Immediate.Validations.Tests/GeneratorTests/CustomValidationTests.AdditionalValidationsInherited#IV...SubClass.g.verified.cs
@@ -0,0 +1,40 @@
+﻿//HintName: IV...SubClass.g.cs
+using System.Collections.Generic;
+using Immediate.Validations.Shared;
+
+#nullable enable
+#pragma warning disable CS1591
+
+
+partial class SubClass
+{
+	static List<ValidationError> IValidationTarget<SubClass>.Validate(SubClass? target) =>
+		Validate(target);
+
+	public static  List<ValidationError> Validate(SubClass? target)
+	{
+		if (target is not { } t)
+		{
+			return 
+			[
+				new()
+				{
+					PropertyName = ".self",
+					ErrorMessage = "`target` must not be `null`.",
+				},
+			];
+		}
+		
+		var errors = new List<ValidationError>();
+
+		errors.AddRange(global::BaseClass.Validate(t));
+
+
+
+		return errors;
+	}
+
+
+
+}
+
