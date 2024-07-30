f79978ccbeba8486dfda90f6ba352b5999035a03
Stuart Turner <stuart@turner-isageek.com>
2024-07-30 09:55:20 -0500

    Fix `[Flags]` enums (#86)

--- tests/Immediate.Validations.FunctionalTests/Validators/EnumValueTests.cs
+++ tests/Immediate.Validations.FunctionalTests/Validators/EnumValueTests.cs
@@ -12,14 +12,28 @@ public sealed partial class EnumValueTests
 		Invalid,
 	}
 
+	[Flags]
+	public enum FlagsState
+	{
+		None = 0,
+		Valid = 0x1,
+		Invalid = 0x2,
+	}
+
 	[Validate]
 	public partial record EnumRecord : IValidationTarget<EnumRecord>
 	{
 		public required ValidState ValidState { get; init; }
 	}
 
+	[Validate]
+	public partial record FlagsEnumRecord : IValidationTarget<FlagsEnumRecord>
+	{
+		public required FlagsState ValidState { get; init; }
+	}
+
 	[Fact]
-	public void StringNotNullTestWhenNotNull()
+	public void EnumValidValue()
 	{
 		var instance = new EnumRecord { ValidState = ValidState.Valid };
 
@@ -29,7 +43,7 @@ public sealed partial class EnumValueTests
 	}
 
 	[Fact]
-	public void StringNotNullTestWhenNull()
+	public void EnumInvalidValue()
 	{
 		var instance = new EnumRecord { ValidState = (ValidState)3 };
 
@@ -41,6 +55,45 @@ public sealed partial class EnumValueTests
 				{
 					PropertyName = nameof(EnumRecord.ValidState),
 					ErrorMessage = "'Valid State' has a range of values which does not include '3'.",
+				}
+			],
+			errors
+		);
+	}
+
+	[Fact]
+	public void FlagsEnumValidValue()
+	{
+		var instance = new FlagsEnumRecord { ValidState = FlagsState.Valid };
+
+		var errors = FlagsEnumRecord.Validate(instance);
+
+		Assert.Empty(errors);
+	}
+
+	[Fact]
+	public void FlagsEnumValidFlags()
+	{
+		var instance = new FlagsEnumRecord { ValidState = FlagsState.Valid | FlagsState.Invalid };
+
+		var errors = FlagsEnumRecord.Validate(instance);
+
+		Assert.Empty(errors);
+	}
+
+	[Fact]
+	public void FlagsEnumInvalidValue()
+	{
+		var instance = new FlagsEnumRecord { ValidState = (FlagsState)4 };
+
+		var errors = FlagsEnumRecord.Validate(instance);
+
+		Assert.Equal(
+			[
+				new()
+				{
+					PropertyName = nameof(EnumRecord.ValidState),
+					ErrorMessage = "'Valid State' has a range of values which does not include '4'.",
 				}
 			],
 			errors
