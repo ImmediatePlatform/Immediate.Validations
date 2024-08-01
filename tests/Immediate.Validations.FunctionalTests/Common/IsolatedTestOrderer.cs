2359d404c2b1de6f186dfd9bfb0b685777664a15
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-08-01 19:06:56 -0400

    Added more tests.

--- tests/Immediate.Validations.FunctionalTests/Common/IsolatedTestOrderer.cs
+++ tests/Immediate.Validations.FunctionalTests/Common/IsolatedTestOrderer.cs
@@ -5,20 +5,6 @@ namespace Immediate.Validations.FunctionalTests.Common;
 
 public sealed class IsolatedTestOrderer : ITestCollectionOrderer
 {
-	public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
-	{
-		var testCollectionsList = testCollections.ToList();
-		var isolatedCollection = testCollectionsList.FirstOrDefault(tc => tc.DisplayName == nameof(IsolatedTestCollectionDefinition));
-		var otherCollections = testCollectionsList.Where(tc => tc.DisplayName != nameof(IsolatedTestCollectionDefinition));
-
-		if (isolatedCollection != null)
-		{
-			yield return isolatedCollection;
-		}
-
-		foreach (var collection in otherCollections)
-		{
-			yield return collection;
-		}
-	}
+	public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections) =>
+		testCollections.OrderBy(tc => tc.DisplayName == nameof(IsolatedTestCollectionDefinition));
 }
