c8fe740dc511d66dc09aeb428a0c84fd45f2cb00
Stuart Turner <stuart@turner-isageek.com>
2025-08-11 11:48:24 -0500

    Add test for missing code coverage (#209)

---
+++ tests/Immediate.Validations.Tests/GeneratorTests/MiscellaneousTests.SkipSelfProperlySkipsSelf#IV...IInterface1.g.verified.cs
@@ -0,0 +1,84 @@
+﻿//HintName: IV...IInterface1.g.cs
+using System.Collections.Generic;
+using Immediate.Validations.Shared;
+
+#nullable enable
+#pragma warning disable CS1591
+
+
+partial interface IInterface1
+{
+	static ValidationResult IValidationTarget<IInterface1>.Validate(IInterface1? target) =>
+		Validate(target, []);
+
+	static ValidationResult IValidationTarget<IInterface1>.Validate(IInterface1? target, ValidationResult errors) =>
+		Validate(target, errors);
+
+	public static new ValidationResult Validate(IInterface1? target) =>
+		Validate(target, []);
+
+	public static new ValidationResult Validate(IInterface1? target, ValidationResult errors)
+	{
+		if (target is not { } t)
+		{
+			return new()
+			{
+				{ ".self", "`target` must not be `null`." },
+			};
+		}
+
+		if (!errors.VisitType(typeof(IInterface1)))
+			return errors;
+		
+
+		__ValidateProperty(errors, t, t.Property);
+
+
+		return errors;
+	}
+
+
+
+	private static void __ValidateProperty(
+		ValidationResult errors, IInterface1 instance, string target
+	)
+	{
+
+		if (target is not { } t)
+		{
+			errors.Add(
+				$"Property",
+				global::Immediate.Validations.Shared.NotNullAttribute.DefaultMessage,
+				new()
+				{
+					["PropertyName"] = $"Property",
+					["PropertyValue"] = null,
+				}
+			);
+
+			return;
+		}
+
+
+
+		{
+			if (!global::Immediate.Validations.Shared.NotEmptyAttribute.ValidateProperty(
+					t
+				)
+			)
+			{
+				errors.Add(
+					$"Property",
+					global::Immediate.Validations.Shared.NotEmptyAttribute.DefaultMessage,
+					new()
+					{
+						["PropertyName"] = $"Property",
+						["PropertyValue"] = t,
+					}
+				);
+			}
+		}
+	}
+
+}
+
---
+++ tests/Immediate.Validations.Tests/GeneratorTests/MiscellaneousTests.SkipSelfProperlySkipsSelf#IV...Implementation.g.verified.cs
@@ -0,0 +1,48 @@
+﻿//HintName: IV...Implementation.g.cs
+using System.Collections.Generic;
+using Immediate.Validations.Shared;
+
+#nullable enable
+#pragma warning disable CS1591
+
+
+partial class Implementation : IValidationTarget
+{
+	ValidationResult IValidationTarget.Validate() =>
+		Validate(this, []);
+
+	ValidationResult IValidationTarget.Validate(ValidationResult errors) =>
+		Validate(this, errors);
+
+	static ValidationResult IValidationTarget<Implementation>.Validate(Implementation? target) =>
+		Validate(target, []);
+
+	static ValidationResult IValidationTarget<Implementation>.Validate(Implementation? target, ValidationResult errors) =>
+		Validate(target, errors);
+
+	public static  ValidationResult Validate(Implementation? target) =>
+		Validate(target, []);
+
+	public static  ValidationResult Validate(Implementation? target, ValidationResult errors)
+	{
+		if (target is not { } t)
+		{
+			return new()
+			{
+				{ ".self", "`target` must not be `null`." },
+			};
+		}
+
+		if (!errors.VisitType(typeof(Implementation)))
+			return errors;
+		
+		global::IInterface1.Validate(t, errors);
+
+
+		return errors;
+	}
+
+
+
+}
+
